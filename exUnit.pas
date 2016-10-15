2fc17e48fb8db57c9c1926fa466067dc6fa09ef9
Capitch <capitchtutoriais@outlook.pt>
2016-10-15 15:41:43 +0100

    Create exUnit.pas

---
+++ exUnit.pas
@@ -0,0 +1,12 @@
+Unit exUnit;
+interface
+  procedure tela;
+  
+implementation
+  procedure tela;
+	begin
+	  writeln('===================');
+	  writeln('  Exemplo de Unit  ');  
+    writeln('===================');
+  end;
+end.	  
